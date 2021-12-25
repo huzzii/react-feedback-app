173fc7f33e3f940f2510ff7309bcd9fb2866f315
huzzii <huzzii466@gmail.com>
2021-12-25 20:06:07 +0530

    initial commit with props and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <FeedbackItem />
+    </>
   );
 }
 
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 5,
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+    },
+    {
+        id: 2,
+        rating: 7,
+        text: 'Lorem of the printing etting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+    },
+    {
+        id: 3,
+        rating: 3,
+        text: 'Lorem Ipsum since the 1500s'
+    },
+]
+
+export default FeedbackData;
